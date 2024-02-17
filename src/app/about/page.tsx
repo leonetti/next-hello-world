124bf4cb3dd98d80a805d1d0af56b38456707c07
Alex Leonetti <alexleonetti.dev@gmail.com>
2024-02-17 14:04:43 -0800

    feat: Route added
    Simple Next Route Handling

---
+++ src/app/about/page.tsx
@@ -0,0 +1,36 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Card, CardBody, Button } from '@nextui-org/react';
+import Image from 'next/image';
+
+import diving from '@public/images/diving.webp';
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <div className="flex justify-center px-4">
+        <Card className="py-4 max-w-full mx-auto my-10">
+          <CardBody className="overflow-visible py-2 text-balance">
+            <div className="flex flex-col gap-6 items-center justify-center text-center">
+              <Image
+                alt="Diving"
+                className="w-80 h-auto object-cover"
+                src={diving}
+              />
+              <div className="flex-1">
+                <h2 className="text-lg font-bold uppercase">Diver</h2>
+                <p className="text-sm text-default-500">
+                  Adventurer &amp; Explorer.
+                </p>
+              </div>
+            </div>
+          </CardBody>
+        </Card>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -6,10 +6,11 @@ import ThemeToggle from './ThemeToggle';
 const Header = () => {
   return (
     <header className="flex justify-between px-4 py-5">
-      <nav className="w-full relative flex items-center">
+      <nav className="w-full relative flex items-center gap-4">
         <Link href="/" className="font-bold text-3xl">
           Hello<span className="text-blue-500">World</span>
         </Link>
+        <Link href="/about">About</Link>
       </nav>
       <ThemeToggle />
     </header>
