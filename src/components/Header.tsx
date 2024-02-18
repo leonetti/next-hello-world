ce28f19d48e856406f4c5e372d3a0a346eea2d94
Alex Leonetti <alexleonetti.dev@gmail.com>
2024-02-17 19:13:02 -0800

    feat: Updated navbar to nextui

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,24 @@
 'use client';
 
+import { useState } from 'react';
 import NextLink from 'next/link';
+import { usePathname } from 'next/navigation';
 import ThemeToggle from '@/components/ThemeToggle';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarMenuToggle,
+  NavbarItem,
+  NavbarMenuItem,
+  NavbarMenu,
+  Link,
+  Button,
+} from '@nextui-org/react';
+import clsx from 'clsx';
 import siteConfig from '@/config/site';
 
-const Logo = () => {
+const SiteLogo = () => {
   return (
     <NextLink href="/" className="text-3xl font-bold">
       Hello<span className="text-blue-500">World</span>
@@ -13,19 +27,67 @@ const Logo = () => {
 };
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const pathname = usePathname();
+
   return (
-    <header className="flex justify-between px-4 py-5">
-      <nav className="w-full relative flex items-center gap-4">
-        <Logo />
-        {siteConfig.navItems.map((navItem) => {
-          return (
-            <NextLink key={navItem.href} href={navItem.href}>
-              {navItem.label}
-            </NextLink>
-          );
-        })}
-      </nav>
-      <ThemeToggle />
+    <header>
+      <Navbar disableAnimation onMenuOpenChange={setIsMenuOpen}>
+        <NavbarContent>
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+            className="sm:hidden"
+          />
+          <NavbarBrand>
+            <SiteLogo />
+          </NavbarBrand>
+        </NavbarContent>
+
+        <NavbarContent className="hidden sm:flex gap-4" justify="center">
+          {siteConfig.navItems.map((item, index) => (
+            <NavbarItem
+              key={`${item.href}-${index}`}
+              isActive={pathname === item.href}
+            >
+              <Link
+                color={pathname === item.href ? 'primary' : 'foreground'}
+                href={item.href}
+                className={clsx({
+                  'px-[1px]': pathname !== item.href,
+                })}
+              >
+                {item.label}
+              </Link>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+
+        <NavbarContent className="hidden xs:flex gap-4" justify="end">
+          <NavbarItem>
+            <Button as={Link} color="primary" href="#" variant="flat">
+              Sign Up
+            </Button>
+          </NavbarItem>
+          <NavbarItem>
+            <ThemeToggle />
+          </NavbarItem>
+        </NavbarContent>
+
+        <NavbarMenu>
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item.href}-${index}`}>
+              <Link
+                color="foreground"
+                className="w-full"
+                href={item.href}
+                size="lg"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </NavbarMenu>
+      </Navbar>
     </header>
   );
 };
